80ba3c745b10c0a7d9a3a37841ab077dc60c0beb
animacpac <56101332+animacpac@users.noreply.github.com>
2021-02-27 14:03:16 -0700

    Hello World done

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require('http');
 
-function sayHello(){
-    console.log("Hello World!");
+function sayHello(req, res){
+    console.log("Recieved a request for: " + req.url);
+
+    res.write("Hello World!!");
+    res.end();
+
 }
 var server = http.createServer(sayHello);
 server.listen(5000);
 
+console.log("The server is listening on port 5000!")
